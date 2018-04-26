c18e8de3a971579579b6c31fd5cf7a8321fd6a2e
Halil Ozan Akgül <hozanakgul@gmail.com>
2018-04-26 02:32:21 +0200

    1 Feature Coded

--- code.py
+++ code.py
@@ -1,8 +1,42 @@
-X = [2, 4, 8]
-Y = [5, 6, 8]
-N = 3
+import csv
+import math
 
-alpha = 0.03
+X=[]#=[100,150,140,300,500,250,400,200,450,350,460,410,320,220,110,280,330,440,170,380]
+Y=[]#=[1000,1150,1100,1500,2000,1400,1650,1230,1600,1650,1800,1680,1640,1300,1200,1440,1490,1820,1230,1600]
+Xt=[]
+Yt=[]
+
+
+with open("RealEstate.csv") as csvfile:
+    reader = csv.reader(csvfile, delimiter = ',')
+    for row in reader:
+        if(row[0]=="MLS" or row[1]!="Santa Maria-Orcutt"):
+            continue
+        X.append(int(float(row[5])))
+        Y.append(int(float(row[2])))
+
+N=len(X)
+Nt = int(N/10)
+
+N -= Nt
+
+Xt = X[N:]
+X = X[:N]
+
+
+Yt = Y[N:]
+Y = Y[:N]
+
+mnx = min(X)
+mxx = max(X)
+
+mny = min(Y)
+mxy = max(Y)
+
+X = [(a/(mxx-mnx)) for a in X]
+Y = [(a/(mxy-mny)) for a in Y]
+
+alpha = 0.0003
 
 m = 1
 n = 0
@@ -10,26 +44,36 @@ n = 0
 def calculateMSE():
     mse = 0
     for i in range(N):
-        mse += (Y[i]-(m*X[i]+n))**2
-    return mse
+        mse += ((Y[i]-(m*X[i]+n))**2)/2
+    return math.sqrt(mse)
+
+def calculateErrorT():
+    ae = 0
+    for i in range(Nt):
+        print(Xt[i], Yt[i], m*Xt[i]+n)
+        ae += (((Yt[i]-(m*Xt[i]+n))/Yt[i])**2)/Nt
+    return math.sqrt(ae)*100
 
 def derM():
     res = 0
     for i in range(N):
         res += -X[i]*(Y[i]-(m*X[i]+n))
-    return res/N
+    return res
 
 def derN():
     res = 0
     for i in range(N):
         res += -(Y[i]-(m*X[i]+n))
-    return res/N
+    return res
 
-print(m, n, calculateMSE())
 
-while calculateMSE()>0.01:
+while abs(derM())>0.00003 and abs(derN())>0.00003:
     newM = m - alpha*derM()
     newN = n - alpha*derN()
     m = newM
     n = newN
-    print(m, n, calculateMSE())
+
+m *= (mxy-mny)/(mxx-mnx)
+n *= (mxy-mny)
+
+print(calculateErrorT())
